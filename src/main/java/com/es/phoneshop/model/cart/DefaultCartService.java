1dd06ca25dec5b867487700ec3a5e7a9e265f281
ElizavetaRudko <eliz.vitebsk@gmail.com>
2020-08-08 01:46:11 +0300

    Task 4.2 - update cart

--- src/main/java/com/es/phoneshop/model/cart/DefaultCartService.java
+++ src/main/java/com/es/phoneshop/model/cart/DefaultCartService.java
@@ -31,15 +31,9 @@ public class DefaultCartService implements CartService {
     }
 
     @Override
-    public synchronized void add(Cart cart, Long productId, int quantity)  throws OutOfStockException {
-        if (quantity <= 0) {
-            throw new OutOfStockException(null, quantity, 0);
-        }
-
+    public synchronized void add(Cart cart, Long productId, int quantity) throws OutOfStockException {
         Product product = productDao.getProduct(productId);
-        Optional<CartItem> cartItemOptional = cart.getItems().stream()
-                .filter(c -> product.getId().equals(c.getProduct().getId()))
-                .findAny();
+        Optional<CartItem> cartItemOptional = getCartItemOptional(cart, quantity, product);
         int productsAmount = cartItemOptional.map(CartItem::getQuantity).orElse(0);
 
         if (product.getStock() < productsAmount + quantity) {
@@ -51,5 +45,27 @@ public class DefaultCartService implements CartService {
         } else {
             cart.getItems().add(new CartItem(product, quantity));
         }
+    }
+
+    @Override
+    public synchronized void update(Cart cart, Long productId, int quantity) throws OutOfStockException {
+        Product product = productDao.getProduct(productId);
+        Optional<CartItem> cartItemOptional = getCartItemOptional(cart, quantity, product);
+
+        if (product.getStock() < quantity) {
+            throw new OutOfStockException(product, quantity, product.getStock());
+        }
+
+        cartItemOptional.ifPresent(cartItem -> cartItem.setQuantity(quantity));
+    }
+
+    private Optional<CartItem> getCartItemOptional(Cart cart, int quantity, Product product) throws OutOfStockException {
+        if (quantity <= 0) {
+            throw new OutOfStockException(null, quantity, 0);
+        }
+
+        return cart.getItems().stream()
+                .filter(c -> product.getId().equals(c.getProduct().getId()))
+                .findAny();
     }
 }
--- src/main/java/com/es/phoneshop/web/CartPageServlet.java
+++ src/main/java/com/es/phoneshop/web/CartPageServlet.java
@@ -1,5 +1,6 @@
 package com.es.phoneshop.web;
 
+import com.es.phoneshop.exception.OutOfStockException;
 import com.es.phoneshop.model.cart.CartService;
 import com.es.phoneshop.model.cart.DefaultCartService;
 
@@ -8,6 +9,11 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
 
 public class CartPageServlet extends AbstractProductServlet {
 
@@ -34,4 +40,60 @@ public class CartPageServlet extends AbstractProductServlet {
         request.setAttribute("cart", cartService.getCart(request.getSession()));
     }
 
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String[] productIds = request.getParameterValues("productId");
+        String[] quantities = request.getParameterValues("quantity");
+        Map<Long, String> errorAttributes = new HashMap<>();
+
+        for (int i = 0; i < productIds.length; i++) {
+            Long productId = getProductIdIfExist(request, response, productIds[i]);
+
+            try {
+                int quantity = getQuantity(quantities[i], request);
+                cartService.update(cartService.getCart(request.getSession()), productId, quantity);
+            } catch (ParseException | OutOfStockException e) {
+                handleError(errorAttributes, productId, e);
+            }
+        }
+
+        if (errorAttributes.isEmpty()) {
+            response.sendRedirect(request.getContextPath() + "/cart?message=Cart updated successfully");
+        } else {
+            request.setAttribute("errors", errorAttributes);
+            doGet(request, response);
+        }
+    }
+
+    private Long getProductIdIfExist(HttpServletRequest request, HttpServletResponse response, String productId) throws IOException {
+        Long id = null;
+        try {
+            id = Long.valueOf(productId);
+        } catch (NumberFormatException ex) {
+            request.setAttribute("message", "Product " + productId + " not found");
+            response.sendError(404);
+        }
+        return id;
+    }
+
+    private void handleError(Map<Long, String> errorAttributes, Long productId, Exception e) {
+        if (e.getClass().equals(ParseException.class)) {
+            errorAttributes.put(productId, "Not a number");
+        } else {
+            if (((OutOfStockException) e).getStockRequested() <= 0) {
+                errorAttributes.put(productId, "Can't be negative or zero");
+            } else {
+                errorAttributes.put(productId, "Out of stock, max available " + ((OutOfStockException) e).getStockAvailable());
+            }
+        }
+    }
+
+    private int getQuantity(String quantityString,  HttpServletRequest request) throws ParseException {
+        NumberFormat numberFormat = getNumberFormat(request.getLocale());
+        return numberFormat.parse(quantityString).intValue();
+    }
+
+    protected NumberFormat getNumberFormat(Locale locale) {
+        return NumberFormat.getInstance(locale);
+    }
 }
